b917f117596157945a68869acfada6ba6bb35623
Sondre Pedersen <sondrp@stud.ntnu.no>
2024-06-05 19:05:00 +0200

    remove helper files

--- fen_formatter.py
+++
@@ -1,9 +0,0 @@
-import re
-
-board = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
-board = board.split(" ")[0]
-board = board.replace("/", "//")
-board = re.sub(r"\d", lambda m: " " * int(m.group(0)), board)
-print("board:")
-print(board + "|")
-
