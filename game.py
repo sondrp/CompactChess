d96f1b4754cf1d00760d9a05e8cbf8fad112f5f2
Sondre Pedersen <sondrp@stud.ntnu.no>
2024-06-05 19:04:34 +0200

    rewrite with less regex

--- game.py
+++ game.py
@@ -2,14 +2,24 @@ from dataclasses import dataclass
 from typing import List, Optional
 import re
 
-# No comments or explanations will be given :)
-# For now at least
+@dataclass
+class State:
+    board: str
+    turn: bool
+    castle: str
+    en_passant: int
+    half_move: int
+    full_move: int
 
-### Data classes
+@dataclass
+class Move:
+    square: int
+    result: str
+    id: str
 
 @dataclass
 class Action:
-    directions: List[str]
+    directions: List[int]
     type: str
     id: Optional[str] = ""
 
@@ -17,234 +27,239 @@ class Action:
 class Piece:
     actions: List[Action]
 
-### Maps
+wPawnAttack = [-9, -11]
+bPawnAttack = [9, 11]
+knight_move = [-19, -8, 12, 21, 19, -8, -12, -21]
+diagonal = [-9, 11, 9, -11]
+orthogonal = [1, 10, -1, -10]
+diag_and_ort = [-9, 1, 11, 10, 9, -1, -11, -10]
 
-paths = {
-    "leaper": (r"^(?:([A-Z])[a-z ]|([a-z])[A-Z ])$", r" \1\2"),
-    "ranger": (r"^(?:([A-Z])( *)[a-z ]|([a-z])( *)[A-Z ])$", r" \2\4\1\3"),
-    "capture": (r"^(?:([A-Z])[a-z]|([a-z])[A-Z])$", r" \1\2"),
-    "pawnForward": (r"^(?:([Pp])(.) )$", r" \2\1"),
-    "pawnDoubleForward": (r"^([Pp])(.. ) $", r" \2\1"),
-    "enPassant": (r"^(?:(P)(..)p |(p)(..)P )$", r" \2\4 \1\3"),
-    "wQueen": (r"^P(.) $", r" \1Q"),
-    "bQueen": (r"^p(.) $", r" \1q"),
-    "castle": (r"^(?:(K) (  ?)(R)(.)|(k) (  ?)(r)(.))$", r" \3\7\1\5\2\6\4\8"),
-    "orthogonal": (r"[A-Z]k|[a-z]K|[A-Z]\s*[qr]|[a-z]\s*[QR]", r""),
-    "diagonal": (r"[A-Z]k|[a-z]K|[A-Z]\s*[qb]|[a-z]\s*[QB]", r""),
-    "bPawnAttack": (r"[A-Z]p", r""),
-    "wPawnAttack": (r"[a-z]P", r""),
-    "knightAttack": (r"[A-Z]n|[a-z]N", r""),
-}
+ranger = re.compile(r"^(?:[A-Z]\s*[a-z ]|[a-z]\s*[A-Z ])")
+leaper = re.compile(r"^(?:[A-Z][a-z ]|[a-z][A-Z ])")
+unoccupied = re.compile(r"^. ")
+capture = re.compile(r"^(?:[A-Z][a-z]|[a-z][A-Z])")
 
-id_to_castle_squares = {
-    "Q": [72, 73, 74],
-    "K": [74, 75, 76],
-    "q": [2, 3, 4],
-    "k": [4, 5, 6],
-}
-
-### Pieces
-
-rook = Piece([Action(["E", "S", "W", "N"], "ranger")])
-knight = Piece([Action(["NNW", "WWN", "WWS", "SSW", "SSE", "EES", "EEN", "NNE"], "leaper")])
-bishop = Piece([Action(["NW", "SW", "SE", "NE"], "ranger")])
-queen = Piece([Action(["NE", "E", "SE", "S", "SW", "W", "NW", "N"], "ranger")])
+knight = Piece([Action(knight_move, leaper)])
+bishop = Piece([Action(diagonal, ranger)])
+queen = Piece([Action(diag_and_ort, ranger)])
 wKing = Piece([
-    Action(["NE", "E", "SE", "S", "SW", "W", "NW", "N"], "leaper", "KQ"),
-    Action(["E E- E 7NW"], "castle", "K"),
-    Action(["W W- W W 7NE"], "castle", "Q"),
+    Action(diag_and_ort, leaper, "KQ"),
+    Action([2], unoccupied, "K"),
+    Action([-2], unoccupied, "Q"),
 ])
 bKing = Piece([
-    Action(["NE", "E", "SE", "S", "SW", "W", "NW", "N"], "leaper", "kq"),
-    Action(["E E- E 7SW"], "castle", "k"),
-    Action(["W W- W W 7SE"], "castle", "q"),
+    Action(diag_and_ort, leaper, "kq"),
+    Action([2], unoccupied, "k"),
+    Action([-2], unoccupied, "q"),
 ])
 wPawn = Piece([
-    Action(["NE", "NW"], "capture"),
-    Action(["NN S"], "pawnForward"),
-    Action(["6S 7N"], "wQueen"),
-    Action(["S 7N 5S N"], "pawnDoubleForward", "pawnDoubleForward"),
-    Action(["4S 7N 3S1W N", "4S 7N 3S1E N"], "enPassant", "enPassant"),
+    Action(wPawnAttack, capture, "P"),
+    Action([-10], unoccupied, "P"),
+    Action([-20], unoccupied, "twoForward"),
+    Action(wPawnAttack, unoccupied, "enPassant"),
 ])
-
 bPawn = Piece([
-    Action(["SE", "SW"], "capture"),
-    Action(["NN S"], "pawnForward"),
-    Action(["6S 7N"], "bQueen"),
-    Action(["N 7S 5N S"], "pawnDoubleForward", "pawnDoubleForward"),
-    Action(["4N 7S 3N1S S", "4N 7S 3N1E S"], "enPassant", "enPassant"),
-])
-cover_piece = Piece([
-    Action(["E", "S", "W", "N"], "orthogonal"),
-    Action(["NE", "SE", "SW", "NW"], "diagonal"),
-    Action(["NE", "NW"], "bPawnAttack"),
-    Action(["SE", "SW"], "wPawnAttack"),
-    Action(["NNE", "EEN", "EES", "SSE", "SSW", "WWS", "WWN", "NNW"], "knightAttack"),
+    Action(bPawnAttack, capture, "p"),
+    Action([10], unoccupied, "p"),
+    Action([20], unoccupied, "twoForward"),
+    Action(bPawnAttack, unoccupied, "enPassant"),
 ])
 
 piece_map = {
-    "R": rook,
-    "r": rook,
-    "N": knight,
-    "n": knight,
-    "B": bishop,
-    "b": bishop,
-    "Q": queen,
-    "q": queen,
-    "K": wKing,
-    "k": bKing,
-    "P": wPawn,
-    "p": bPawn,
+    'R': Piece([Action(orthogonal, ranger, "R")]),
+    'r': Piece([Action(orthogonal, ranger, "r")]),
+    'B': bishop,
+    'b': bishop,
+    'N': knight,
+    'n': knight,
+    'Q': queen,
+    'q': queen,
+    'K': wKing,
+    'k': bKing,
+    'P': wPawn,
+    'p': bPawn,
 }
 
-### Functions
+def generate_moves(board, index, action: Action, include_board=True) -> List[Move]:
+    results = []
 
-def is_square_covered(board, square):
+    for offset in action.directions:
+        path = board[index::offset]
+        match = re.match(action.type, path) 
+        if not match: continue
+
+        path = match.group(0)
+        for i in range(1, len(path)):
+            landing_square = index + offset * i
+
+            if not include_board:
+                results.append(Move(landing_square, "", ""))
+                continue
+
+            board_result = list(board)
+            board_result[index] = " "
+            board_result[landing_square] = path[0]
+            results.append(Move(landing_square, "".join(board_result), action.id))
+
+    return results
+
+white_cover_piece = Piece([
+    Action(wPawnAttack, r".P"),
+    Action(knight_move, r".N"),
+    Action(diag_and_ort, r".K"),
+    Action(orthogonal, r".\s*[QR]"),
+    Action(diagonal, r".\s*[QB]"),
+])
+black_cover_piece = Piece([
+    Action(bPawnAttack, r".p"),
+    Action(knight_move, r".n"),
+    Action(diag_and_ort, r".k"),
+    Action(orthogonal, r".\s*[qr]"),
+    Action(diagonal, r".\s*[qb]"),
+])
+
+
+def is_square_covered(board, index, by_white):
+    cover_piece = white_cover_piece if by_white else black_cover_piece 
     for action in cover_piece.actions:
-        for direction in action.directions:
-            for move in generate_moves(board, square, action, direction):
-                if board[move[0]] != " ": return True
+        for move in generate_moves(board, index, action, False):
+            if board[move.square] != " ": return True
     return False
 
-def castle_filter(board, move):
-    id = move[2]
-    if not id or id not in "KQkq": return True
-    castle_squares = id_to_castle_squares[move[2]]
-    for square in castle_squares:
-        if (is_square_covered(board, square)):
-            return True
-    return False
-
-def state_filter(move, en_passant, castle):
-    id = move[2]
-    
-    if id == "enPassant" and move[0] != en_passant:
-        return False
-    
-    if id in "KQkq" and id not in castle:
-        return False
-    return True
-
-def check_filter(board, white_king):
+def is_king_in_check(board, white_king):
     king_to_find = "K" if white_king else "k"
     king_index = board.find(king_to_find)
-    return not is_square_covered(board, king_index)
+    return is_square_covered(board, king_index, not white_king)
 
-def parse_direction(match):
-    direction, click_spec = match.groups()
-    offset = 0
+# Return false only if the move is attempting to en passant illegally.
+def en_passant_filter(state: State, move: Move):
+    return move.id != "enPassant" or move.square == state.en_passant
 
-    for m in re.finditer(r"(\d?)([ESWN]+)", direction):
-        reps, num = m.groups()
-        reps = int(reps) if reps.isdecimal() else 1
-        num = 10 * num.count("S") + num.count("E") - 10 * num.count("N") - num.count("W")
-        offset += reps * num
 
-    return offset, bool(click_spec)
 
-def generate_moves(board, index, action, direction):
-    path_regex, replacement = paths[action.type]
+def castle_filter(state: State, move: Move):
+    square, result, id = move.square, move.result, move.id
+    if id == "" or id not in "QKqk": return True
 
-    current_square = index
-    current_path = board[index]
-    visited = [index]
-    square_to_click = None
+    castle = state.castle
+    if id not in castle: return False
 
-    moves = []
+    index = "QKqk".find(id)
 
-    for sub_direction in re.finditer(r"([ESWN\d]+)(-?)", direction):
+    castle_squares = [[72, 73, 74], [74, 75, 76], [2, 3, 4], [4, 5, 6]][index]
 
-        offset, should_specify_click = parse_direction(sub_direction)
+    for square in castle_squares:
+        if (is_square_covered(result, square, not state.turn)):
+            return False
 
-        while (True):
+    result = list(move.result)
+    replacements = [" ", "R", "K"] if state.turn else [" ", "r", "k"]
+    for i, square in enumerate(castle_squares):
+        result[square] = replacements[i]
+    move.result = "".join(result)
+    return True
 
-            current_square += offset
-            if current_square < 0 or 77 < current_square: return moves 
-            current_path += board[current_square]
-            visited.append(current_square)
-            if (should_specify_click):
-                square_to_click = current_square
+def in_check_filter(state: State, move: Move):
+    square, result = move.square, move.result
+    white_move = result[square].isupper()
+    return not is_king_in_check(result, white_move)
 
-            resulting_path = re.sub(path_regex, replacement, current_path)
+def pawn_double_forward_filter(state: State, move: Move):
+    square, result, id = move.square, move.result, move.id
+    if id != "twoForward": return True
 
-            if (resulting_path == current_path): break
+    white_move = state.turn
+    row = 4 if white_move else 3
+    if square // 10 != row: return False
 
-            result = list(board)
-            for i in range(len(visited)):
-                result[visited[i]] = resulting_path[i]
+    square_to_check = square + (10 if white_move else -10)
+    return result[square_to_check] == " "
 
-            square_to_click = square_to_click if square_to_click else current_square
-            moves.append((square_to_click, "".join(result), action.id))
-            square_to_click = None
-    return moves
+def generate_legal_moves(state, index, include_board=True) -> List[Move]:
+    if index % 10 > 7: return []
+    board = state.board
+    piece = board[index]
+    if piece == " ":
+        return []
+
+    if piece.isupper() != state.turn:
+        return []
+        
+    piece = board[index]
+    piece = piece_map[piece]
+
+    legal_moves = []
+    for action in piece.actions:
+        legal_moves += generate_moves(board, index, action, include_board)
+
+    legal_moves = list(filter(lambda m: pawn_double_forward_filter(state, m), legal_moves))
+    legal_moves = list(filter(lambda m: castle_filter(state, m), legal_moves))
+    legal_moves = list(filter(lambda m: en_passant_filter(state, m), legal_moves))
+    return list(filter(lambda m: in_check_filter(state, m), legal_moves))
+
+def update_state(state: State, move: Move):
+    square, result, id = move.square, move.result, move.id
+    
+    if id == "twoForward":
+        state.en_passant = square + (10 if state.turn else -10)
+    else: 
+        state.en_passant = -1
+    
+
+    if re.match(r"[KQkq]+", id):
+        state.castle = "".join([l for l in state.castle if l not in id])
+
+    # lord have mercy...
+    if result[0] != "R": state.castle = state.castle.replace("Q", "")
+    if result[7] != "R": state.castle = state.castle.replace("K", "")
+    if result[70] != "r": state.castle = state.castle.replace("q", "")
+    if result[77] != "r": state.castle = state.castle.replace("k", "")
+
+    state.board = result
+    state.half_move += 1
+    state.full_move += 0 if state.turn else 1 
+    state.turn = not state.turn
+
+def check_game_active(state: State):
+    return any(generate_legal_moves(state, square) for square in range(78))
 
 class Chess:
-    board = "rnbqkbnr//pppppppp//        //        //        //        //PPPPPPPP//RNBQKBNR"
-    en_passant = -1
-    castle = "KQkq"
-    legal_moves = []
-    turn = True
 
-    def click(self, index):
-        move = next((move for move in self.legal_moves if move[0] == index), None)
+    legal_moves: List[Move] = []
+    game_active = True
+
+    def __init__(self, fen="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"):
+        board, turn, castle, en_passant, half_move, full_move = fen.split(" ")
+        board = board.replace("/", "//")
+        board = re.sub(r"\d", lambda m: " " * int(m.group(0)), board)
+        turn = turn == "w"
+        en_passant = int(en_passant) if en_passant.isdigit() else -1
+        self.state = State(board, turn, castle, en_passant, int(half_move), int(full_move))
+
+    def click(self, square):
+        state = self.state
+        move = next((move for move in self.legal_moves if move.square == square), None)
 
         if move:
-            square, board, id = move
-            self.turn = not self.turn
-            self.board = board
-            if id == "enPassant":
-                offset = -10 if board[square].isupper() else 10
-                self.en_passant = square + offset
-            
-            if id in self.castle:
-                self.castle.replace(id, "")
-            
+            update_state(state, move)
+            self.legal_moves = []
+            self.game_active = check_game_active(state)
             return True
 
-        self.legal_moves = self.calculate_legal_moves(index)
+        self.legal_moves = generate_legal_moves(state, square)
         return False
-
-    def calculate_legal_moves(self, index):
-        if index < 0 or 77 < index: return []
-        piece = self.board[index]
-
-        legal_moves = []
-
-        if piece == " " or self.turn == piece.islower():
-            return legal_moves
-
-        piece = piece_map[piece]  
-
-        for action in piece.actions:
-            for direction in action.directions:
-                legal_moves += generate_moves(self.board, index, action, direction)
-        legal_moves = filter(lambda m: check_filter(self.board, self.turn), legal_moves)
-        legal_moves = filter(lambda m: state_filter(m, self.en_passant, self.castle), legal_moves)
-        legal_moves = filter(lambda m: castle_filter(self.board, m), legal_moves)
-        return list(legal_moves)
-
-    def check_game_over(self):
-        for index, piece in enumerate(self.board):
-            if piece == " " or piece == "/": continue
-            if self.turn != piece.isupper(): continue
-            piece = piece_map[piece]
-            for action in piece.actions:
-                for direction in action.directions:
-                    if generate_moves(self.board, index, action, direction):
-                        return False
-        return True
-
+    
     def print(self):
-        print(self.board.replace("//", "\n"))
+        print(self.state.board.replace("//", "\n"))
 
 def main():
     chess = Chess()
-    while (True):
+    while (chess.game_active):
         square = int(input("Square: "))
         if (chess.click(square)):
             chess.print()
         else:
-            print([m[0] for m in chess.legal_moves])
+            print([m.square for m in chess.legal_moves])
 
 if __name__ == "__main__":
     main()
