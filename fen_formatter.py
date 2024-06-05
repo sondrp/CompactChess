90a56fb8c5f4aa1a8a2c8888f0c790f617845d5f
Sondre Pedersen <sondrp@stud.ntnu.no>
2024-06-05 12:43:34 +0200

    working simple result generator

--- fen_formatter.py
+++ fen_formatter.py
@@ -1,6 +1,6 @@
 import re
 
-board = "1rrrrrr1/8/7n/p2R3n/7n/6nn/5n2/8 w - - 0 1"
+board = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 board = board.split(" ")[0]
 board = board.replace("/", "//")
 board = re.sub(r"\d", lambda m: " " * int(m.group(0)), board)
--- rowgrabber.py
+++ rowgrabber.py
@@ -1,101 +1,47 @@
 import re
-board = " rrrrrr //        //       n//p  RR  n//       n//      nn//     n  //        "
-
-## Hmm... you can get the index of a match... For each match. Is that going to be useful? Let us find out!
-## Tasks:
-# Extract row based on row number - CHECK
-# Extract column based on column number - CHECK
-# Extract diagonal based on row and column number
-# Extract directions from some starting point
-# Extract directions from starting point until condition is met
-
-# Do I want to extract all the indices too? Not yet.
 print("start (remove this line later)")
 
-index = 33
-offsets = [1, 10, -1, -10]
-path_regex = re.compile(r"^(?:[A-Z]\s*[a-z ]|[a-z]\s*[A-Z ])")
 
-results = []
+board = "rnbqkbnr//pppppppp//        //        //        //        //PPPPPPPP//RNBQKBNR"
+index = 71
+ranger = re.compile(r"^(?:[A-Z]\s*[a-z ]|[a-z]\s*[A-Z ])")
+leaper = re.compile(r"^(?:[A-Z][a-z ]|[a-z][A-Z ])")
 
-for offset in offsets:
-    path = board[index::offset]
-    match = re.match(path_regex, path) 
-    if not match: continue
-
-    # valid path confirmed. Each char in the path (after one) is a possible move.
-    for i in range(1, len(path)):
-        landing_square = index + offset * i
-        board_result = list(board)
-        board_result[index] = " "
-        board_result[landing_square] = path[0]
-        results.append(landing_square, "".join(board_result))
-
-
-def extract_paths(board, index, offsets):
-    return [board[index::offset] for offset in offsets]
-
-
-#path_replace_regex = re.compile()
-
-paths = extract_paths(board, index, offsets)
-
-for path in paths:
-    match = re.match(path_regex, path)
-    if not match: continue
-
-    path = match.group(0)
-    piece = path[0]
+rook = (
+    [1, 10, -1, -10],
+    ranger
+)
+knight = (
+    [-19, -8, 12, 21, 29, 8, -12, -21],
+    leaper
+)
+def generate_results(board, index, path_regex, offsets, include_board=True):
     results = []
-    for i in range(1, len(path)):
 
+    for offset in offsets:
+        path = board[index::offset]
+        match = re.match(path_regex, path) 
+        if not match: continue
 
-        board_result = list(board)
-        board_result[index] = " "
-        board_result[index]
+        path = match.group(0)
+        for i in range(1, len(path)):
+            landing_square = index + offset * i
 
+            if not include_board:
+                results.append(landing_square)
+                continue
 
-        # path_result = list(path)
-        # path_result[0] = " "
-        # path_result[landing_square] = piece
+            board_result = list(board)
+            board_result[index] = " "
+            board_result[landing_square] = path[0]
+            results.append((landing_square, "".join(board_result)))
 
-        # at this point I have a path. The landing square is of course the click square
-        # now to make the board. 
-        # of course, in simple terms I don't need to create the path at all. 
-        # just put the move directly in there. Makes life a lot easier, but will not do in 
-        # the long term. Let us do it for now
+    return results
 
+# Ok, great. I am now able to do the simple stuff. 
+# What about the hard stuff? 
+# Castle for example. The path replacement will be slightly more challenging
+# Similarly, the board reconstruction is harder too.
 
-        
-paths = map(lambda p: re.match(path_regex, p), paths)
-
-for m in paths:
-    if not m: continue
-    path = m.group(0)
-    piece = path[0]
-    results = []
-    for i in range(1, len(path)):
-        result = list(path)
-        result[0] = " "
-        result[i] = path[0]
-        results.append("".join(result))
-
-# all paths are now generated. Time to create the boards for each one.
-boards = []
-for i in range(len(offsets)):
-    board_result = list(board)
-    offset = offsets[i]
-    path_result = results[i]
-    for y in range(len(path_result)):
-        board_result[index + offset * y] = path_result[y]
-    boards.append("".join(board_result))
-    
-
-    print("lolas")
-
-
-
-
-
-print(paths)
-
+# for square, res in generate_results(board, index, knight[1], knight[0]):
+#     print(square, res.replace("/", ""))
