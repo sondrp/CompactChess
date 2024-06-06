ed3dbfa1f6bac2fd00975a8f46c1e6921153d5a7
Sondre Pedersen <sondrp@stud.ntnu.no>
2024-06-06 12:50:06 +0200

    allow cors

--- backend/endpoint.py
+++ backend/endpoint.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI, Header
+from fastapi.middleware.cors import CORSMiddleware
 from game import Chess
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 chess = Chess()
 
 @app.get("/")
